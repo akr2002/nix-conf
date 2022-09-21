a02f9dac8f67850ea3cd945da6dbeffe1ea308f5
Aditya <bluenerd@protonmail.com>
2022-09-21 16:13:19 +0530

    new commit

---
+++ home.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "user";
+  home.homeDirectory = "/home/user";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = [
+	pkgs.wget
+	pkgs.neofetch
+	pkgs.htop
+	pkgs.oh-my-zsh
+	pkgs.git
+	pkgs.chromium
+	pkgs.gdb
+	pkgs.clang
+	pkgs.lldb
+	pkgs.kitty
+  ];
+
+	programs.bash = {
+		enable = true;
+		bashrcExtra = ''
+			. ~/bashrc
+		'';	
+	};
+
+	programs.zsh = {
+		enable = true;
+		enableCompletion = true;
+		enableAutosuggestions = true;
+		enableSyntaxHighlighting = true;
+		oh-my-zsh = {
+			enable = true;
+			theme = "ys";
+			plugins = ["git" "colored-man-pages" "extract" "sudo"];
+		};
+	};
+
+}
