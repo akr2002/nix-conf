dc897875f0f1f24f9b231fbc947426d28badf77b
Aditya <bluenerd@protonmail.com>
2023-05-22 23:05:29 +0530

    remove nvim default home-manager

--- home.nix
+++ home.nix
@@ -64,7 +64,6 @@
     };
   };
 
-  environment.variables.EDITOR = "nvim"
 
 #  programs.neovim = {
 #    enable = true;
@@ -73,7 +72,7 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+  #home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -84,7 +83,7 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  };
+  #};
 
   # You can also manage environment variables but you will have to manually
   # source
