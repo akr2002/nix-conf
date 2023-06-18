32fb60453691989c34f8c80283a507856f751e89
Aditya <bluenerd@protonmail.com>
2023-06-18 11:00:23 +0530

    add ranger

--- home.nix
+++ home.nix
@@ -56,7 +56,7 @@
     xsel wget neofetch htop oh-my-zsh git chromium plasma5Packages.qtstyleplugin-kvantum kdeconnect
     bat microsoft-edge vivaldi vivaldi-ffmpeg-codecs x264 nerdfonts joypixels rustc cargo unzip ocs-url
     nextcloud-client duf neovim foliate mpv obsidian dino aria2 bitwarden kitty-themes p7zip
-    tdesktop libreoffice-fresh jetbrains-mono yakuake krita filelight inkscape yt-dlp
+    tdesktop libreoffice-fresh jetbrains-mono yakuake krita filelight inkscape yt-dlp ranger
     libsForQt5.kwallet libsForQt5.kwallet-pam libsForQt5.kwalletmanager libsForQt5.ksshaskpass
   ];
 
