1dd61b98e1b429d107d4292d13955f26f0104642
Aditya <bluenerd@protonmail.com>
2023-07-01 12:21:12 +0530

    use flakes

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.bridge = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
