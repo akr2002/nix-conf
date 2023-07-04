2902aca32aaceaff4e11d32e8eedf9f1c531ac11
Aditya <bluenerd@protonmail.com>
2023-07-04 09:20:53 +0530

    add astronvim config

--- home.nix
+++ home.nix
@@ -111,6 +111,31 @@
  programs.neovim = {
    enable = true;
    defaultEditor = true;
+   coc.enable = true;
+   extraLuaConfig = ''
+    for _, source in ipairs {
+	    "astronvim.bootstrap",
+	    "astronvim.options",
+	    "astronvim.lazy",
+	    "astronvim.autocmds",
+	    "astronvim.mappings",
+	  } do
+	    local status_ok, fault = pcall(require, source)
+	    if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault
+	    ) end
+	  end
+
+	  if astronvim.default_colorscheme then
+	  	if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+		  requrie("astronvim.utils").notify(
+		    "Error setting up colorscheme: " .. astronvim.default_colorscheme,
+		    vim.log.levels.ERROR
+		  )
+		  end
+	  end
+
+	  require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+      '';
 
    plugins = (with pkgs.vimPlugins; [
    rust-vim
