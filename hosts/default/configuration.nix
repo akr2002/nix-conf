157e7143dc45524b73679ab160ff4eff59a6e4a8
Aditya <bluenerd@protonmail.com>
2024-04-10 22:54:03 +0530

    add filesystem

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -43,16 +43,6 @@
   #   "/crypto_keyfile.bin" = null;
   # };
 
-  fileSystems."/".options = ["noatime" "nodiratime" "discard"];
-  # Enable swap on luks
-  boot.initrd.luks.devices."luks-3b571a97-616b-4dd7-9abb-d1e0491d178a".device = "/dev/disk/by-uuid/3b571a97-616b-4dd7-9abb-d1e0491d178a";
-  # boot.initrd.luks.devices."luks-c0ef1093-a74e-4491-8c73-0009d3377c19".keyFile = "/crypto_keyfile.bin";
-  boot.initrd.luks.devices."luks-3b571a97-616b-4dd7-9abb-d1e0491d178a".allowDiscards = true;
-
-  # Allow discard on /
-  boot.initrd.luks.devices."luks-a09877ce-f520-429a-9f3b-57b073e4d662".device = "/dev/disk/by-uuid/a09877ce-f520-429a-9f3b-57b073e4d662";
-  boot.initrd.luks.devices."luks-a09877ce-f520-429a-9f3b-57b073e4d662".allowDiscards = true;
-
   networking.hostName = "bridge"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
