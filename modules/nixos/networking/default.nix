1d63a462910fb8070f5081ac8c3f8defd7a84970
Aditya <bluenerd@protonmail.com>
2024-04-11 11:11:14 +0530

    add networking option

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -1,23 +1,32 @@
-_: {
-  networking = {
-    hostname = "bridge";
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    networking.enable = lib.mkEnableOption "enable networking";
+  };
+  config = lib.mkIf config.networking.enable {
+    networking = {
+      hostname = "bridge";
 
-    networkmanager = {
-      enable = true;
-      unmanaged = ["virbr0" "docker0"];
+      networkmanager = {
+        enable = true;
+        unmanaged = ["virbr0" "docker0"];
+      };
+
+      nftables.enable = true;
+
+      firewall = {
+        allowedTCPPorts = [80 443];
+        trustedInterfaces = ["incusbr0" "virbr0"];
+      };
+
+      extraHosts = ''
+        185.199.108.133 raw.githubusercontent.com
+      '';
+      nameservers = ["1.1.1.1" "9.9.9.9"];
+      bridges = {incusbr0.interfaces = [];};
     };
-
-    nftables.enable = true;
-
-    firewall = {
-      allowedTCPPorts = [80 443];
-      trustedInterfaces = ["incusbr0" "virbr0"];
-    };
-
-    extraHosts = ''
-      185.199.108.133 raw.githubusercontent.com
-    '';
-    nameservers = ["1.1.1.1" "9.9.9.9"];
-    bridges = {incusbr0.interfaces = [];};
   };
 }
