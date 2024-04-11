7dfe959bcc1d310330f92ee53adaaa8320044e1a
Aditya <bluenerd@protonmail.com>
2024-04-11 11:23:51 +0530

    add virtualisation option

--- modules/nixos/virtualisation/default.nix
+++ modules/nixos/virtualisation/default.nix
@@ -1,12 +1,21 @@
-_: {
-  virtualisation = {
-    docker.enable = true;
-    incus.enable = true;
-    libvirtd.enable = true;
-    lxc = {
-      enable = true;
-      lxcfs.enable = true;
+{
+  config,
+  libs,
+  ...
+}: {
+  options = {
+    virtualisation.enable = lib.mkEnableOption "enable virtualisation";
+  };
+  config = lib.mkIf config.virtualisation.enable {
+    virtualisation = {
+      docker.enable = true;
+      incus.enable = true;
+      libvirtd.enable = true;
+      lxc = {
+        enable = true;
+        lxcfs.enable = true;
+      };
+      waydroid.enable = true;
     };
-    waydroid.enable = true;
   };
 }
