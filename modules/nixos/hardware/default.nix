ea040a6d5e494ff89e74169dbbc3de58861b3bb9
Aditya <bluenerd@protonmail.com>
2024-04-11 11:20:39 +0530

    add hardware option

--- modules/nixos/hardware/default.nix
+++ modules/nixos/hardware/default.nix
@@ -1,12 +1,22 @@
-{pkgs, ...}: {
-  hardware = {
-    #firmware = with pkgs; [firmwareLinuxNonfree];
-    opengl = {
-      enable = true;
-      extraPackages = with pkgs; [vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver];
-    };
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options = {
+    hardware.enable = lib.mkEnableOption "enable hardware options";
   };
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+  config = lib.mkIf config.hardware.enable {
+    hardware = {
+      #firmware = with pkgs; [firmwareLinuxNonfree];
+      opengl = {
+        enable = true;
+        extraPackages = with pkgs; [vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver];
+      };
+    };
+    nixpkgs.config.packageOverrides = pkgs: {
+      vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+    };
   };
 }
