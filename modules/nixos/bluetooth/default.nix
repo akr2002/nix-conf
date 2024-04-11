9177bf6c020843e55bcbc8223fb5683fdd036a92
Aditya <bluenerd@protonmail.com>
2024-04-11 11:13:35 +0530

    add bluetooth option

--- modules/nixos/bluetooth/default.nix
+++ modules/nixos/bluetooth/default.nix
@@ -1,10 +1,19 @@
-_: {
-  hardware.bluetooth = {
-    enable = true;
-    settings = {
-      General = {
-        Experimental = "true";
-        Enable = "Source,Sink,Media,Socket";
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    bluetooth.enable = lib.mkEnableOption "enable bluetooth";
+  };
+  config = lib.mkIf config.bluetooth.enable {
+    hardware.bluetooth = {
+      enable = true;
+      settings = {
+        General = {
+          Experimental = "true";
+          Enable = "Source,Sink,Media,Socket";
+        };
       };
     };
   };
