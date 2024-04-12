96af961cbb346b50d59b985ba8ed35ebd2b9ce4a
Aditya <bluenerd@protonmail.com>
2024-04-12 12:40:00 +0530

    add opengl settings

--- modules/nixos/hardware/default.nix
+++ modules/nixos/hardware/default.nix
@@ -8,15 +8,19 @@
     hardware.enable = lib.mkEnableOption "enable hardware options";
   };
   config = lib.mkIf config.hardware.enable {
+    environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
     hardware = {
       #firmware = with pkgs; [firmwareLinuxNonfree];
       opengl = {
         enable = true;
-        extraPackages = with pkgs; [vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver];
+        driSupport = true;
+        driSupport32Bit = true;
+        extraPackages = with pkgs; [vaapiIntel vaapiVdpau libvdpau-va-gl intel-media-driver intel-vaapi-driver rocm-opencl-icd rocm-opencl-runtime];
       };
     };
     nixpkgs.config.packageOverrides = pkgs: {
       vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+      intel-vaapi-driver = pkgs.intel-vaapi-driver.override {enableHybridCodec = true;};
     };
   };
 }
