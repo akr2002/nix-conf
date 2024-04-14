64ab66c238aaa426bc7c9b53be367fdbd2c47876
Aditya <bluenerd@protonmail.com>
2024-04-14 16:15:50 +0530

    add gtk theme

--- modules/home-manager/gnome-utils/default.nix
+++ modules/home-manager/gnome-utils/default.nix
@@ -10,12 +10,33 @@
   config = lib.mkIf config.gnome-utils.enable {
     home.packages =
       (with pkgs; [
-        gnome.gnome-tweaks
         gnome-extension-manager
+        gtk-engine-murrine
+      ])
+      ++ (with pkgs.gnome; [
+        gnome-tweaks
+        gnome-themes-extra
       ])
       ++ (with pkgs.gnomeExtensions; [
         user-themes
         blur-my-shell
       ]);
+    gtk = {
+      enable = true;
+      iconTheme = {
+        package = pkgs.tela-icon-theme;
+        name = "Tela-green";
+      };
+      theme = {
+        package = pkgs.orchis-theme.overrideAttrs {
+          installPhase = ''
+            runHook preInstall
+            bash install.sh -d $out/share/themes -t all -c dark -l --tweaks macos --tweaks solid
+            runHook postInstall
+          '';
+        };
+        name = "Orchis-Green-Dark";
+      };
+    };
   };
 }
