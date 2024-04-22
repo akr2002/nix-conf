c92794bf3ec2b673a77630d7aaaefb18ca43ea11
Aditya <bluenerd@protonmail.com>
2024-04-22 11:52:02 +0530

    rm ollama

--- modules/nixos/packages/default.nix
+++ modules/nixos/packages/default.nix
@@ -1,14 +1,10 @@
 {pkgs, ...}: {
-  environment.systemPackages =
-    (with pkgs; [
-      vim
-      wget
-      git
-      nh
-      nix-output-monitor
-      nvd
-    ])
-    ++ (with pkgs.masterPkgs; [
-      ollama
-    ]);
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    nh
+    nix-output-monitor
+    nvd
+  ];
 }
