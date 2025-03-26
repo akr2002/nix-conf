05f33a14a87f9a2ef4bfc5018d3f6452775d0438
Aditya <bluenerd@protonmail.com>
2025-03-26 09:29:18 +0530

    rm env var

--- modules/nixos/environment/default.nix
+++ modules/nixos/environment/default.nix
@@ -1,10 +1,14 @@
-_: {
+{
+lib,
+pkgs,
+...
+}: {
   environment = {
     homeBinInPath = true; # Include ~/bin/ in $PATH
     localBinInPath = true; # Include ~/.local/bin in $PATH
     sessionVariables = {
       # tell electron apps to use wayland
-       NIXOS_OZONE_WL = "1";
+      NIXOS_OZONE_WL = "1";
     };
   };
 }
